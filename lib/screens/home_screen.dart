93d3a6c50aa96d388d523bde46e93d6e5e0e5fb6
Rajnikant Behera <rajnikantb2002@gmail.com>
2026-06-09 19:32:37 +0530

    Implement alarm list display in HomeScreen using Consumer for state management. Updated UI to show alarms with toggle functionality and improved styling for better user experience.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
+import 'package:alarm_clock_app/providers/alarm_provider.dart';
 import 'package:alarm_clock_app/screens/add_edit_alarm_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -8,11 +10,41 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Alarm Clock'), centerTitle: true),
-      body: const Center(
-        child: Text(
-          'No Alarms Yet',
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
+      body: Consumer<AlarmProvider>(
+        builder: (context, provider, child) {
+          if (provider.alarms.isEmpty) {
+            return const Center(
+              child: Text('No Alarms Yet', style: TextStyle(fontSize: 18)),
+            );
+          }
+
+          return ListView.builder(
+            itemCount: provider.alarms.length,
+            itemBuilder: (context, index) {
+              final alarm = provider.alarms[index];
+
+              return Card(
+                margin: const EdgeInsets.all(10),
+                child: ListTile(
+                  title: Text(
+                    alarm.time,
+                    style: const TextStyle(
+                      fontSize: 22,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(alarm.label),
+                  trailing: Switch(
+                    value: alarm.isEnabled,
+                    onChanged: (value) {
+                      provider.toggleAlarm(index, value);
+                    },
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
